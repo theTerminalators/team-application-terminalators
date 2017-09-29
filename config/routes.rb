f25e1e19c7239bc34ad58dcaf7f5230918fe4e8d
jasmine <jasliu2012@gmail.com>
2017-09-28 19:05:47 -0500

    Added show to route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'pets/index'
 
   get '/pages' => 'pages#index' 
-  
+
+  get '/pages' => 'pages#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
